04776e6aefaedcf47edbaf7daf7e62e90dcff63f
Mauricio Fragale <mfragale@gmail.com>
2023-11-17 15:29:54 -0300

    update

--- functions.php
+++ functions.php
@@ -16,6 +16,48 @@ add_filter('woocommerce_enable_order_notes_field', '__return_false');
 
 
 
+/**
+ *	This will hide the Divi "Project" post type.
+ *	Thanks to georgiee (https://gist.github.com/EngageWP/062edef103469b1177bc#gistcomment-1801080) for his improved solution.
+ */
+add_filter('et_project_posttype_args', 'mytheme_et_project_posttype_args', 10, 1);
+function mytheme_et_project_posttype_args($args)
+{
+    return array_merge($args, array(
+        'public'              => false,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => false,
+        'show_in_nav_menus'   => false,
+        'show_ui'             => false
+    ));
+}
+
+
+
+/**
+ * Pre-populate Woocommerce checkout fields
+ */
+add_filter('woocommerce_checkout_get_value', function ($input, $key) {
+    global $current_user;
+    switch ($key):
+        case 'billing_first_name':
+        case 'shipping_first_name':
+            return $current_user->first_name;
+            break;
+
+        case 'billing_last_name':
+        case 'shipping_last_name':
+            return $current_user->last_name;
+            break;
+        case 'billing_email':
+            return $current_user->user_email;
+            break;
+        case 'billing_phone':
+            return $current_user->phone;
+            break;
+    endswitch;
+}, 10, 2);
+
 
 // Nova College MODIFIED
 // add_filter('tutor_student_registration_required_fields', 'required_cpf_no_callback');
